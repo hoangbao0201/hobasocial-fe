c2b4e9fd2ad2de98a7a4d426e3893db51219e18b
Nguyễn Hoàng Bảo <hoangbao020103@gmail.com>
2023-06-14 20:18:29 +0700

    Update constant.js
    Update

--- src/context/constant.js
+++ src/context/constant.js
@@ -1,9 +1,14 @@
 // export const apiUrl = "http://hoangbao-hobachat.herokuapp.com";
 // export const apiUrl = "http://localhost:5000";
 
+// export const apiUrl =
+//     process.env.NODE_ENV !== "production"
+//         ? "http://localhost:5000"
+//         : process.env.URLAPI;
+
 export const apiUrl =
-    process.env.NODE_ENV !== "production"
-        ? "http://localhost:5000"
-        : process.env.URLAPI;
+    process.env.NODE_ENV == "production"
+    ? process.env.URLAPI
+    : "http://localhost:5000"
 
 export const LOCAL_STORAGE_TOKEN_NAME = "TokenUser";
